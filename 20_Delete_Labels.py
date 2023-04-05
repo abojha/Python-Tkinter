38b57d2ddfba03f37c73c75fa4647a1d6b5feaee
abojha <abojha0012@gmail.com>
2023-04-05 12:17:17 +0530

    get rid off delete buttons

--- 20_Delete_Labels.py
+++ 20_Delete_Labels.py
@@ -3,33 +3,46 @@ from random import randint
 
 
 root = Tk()
-root.geometry("600x400")
+root.geometry("400x400")
 root.title("Delete Labels")
 
-def functionDelete():
-    # myLabel.pack_forget()
-    myLabel.destroy()
-    myButton['state'] = NORMAL
+myLabel = Label(root)
+
+# def functionDelete():
+#     myLabel.grid_forget()
+#     # myLabel.destroy()
+#     myButton['state'] = NORMAL
 
 
+# Pack system
+# def functionClick():
+#     global myLabel
+#     hello = "Hello " + e.get()
+#     myLabel = Label(root, text=hello)
+#     e.delete(0, 'end')
+#     myLabel.pack(pady=10)
+#     myButton['state'] = DISABLED
+
+# Grid System
 def functionClick():
     global myLabel
+    myLabel.destroy()
     hello = "Hello " + e.get()
     myLabel = Label(root, text=hello)
     e.delete(0, 'end')
-    myLabel.pack(pady=10)
-    myButton['state'] = DISABLED
+    myLabel.grid(row = 3, column = 0, pady=10)
+    # myButton['state'] = DISABLED
 
 
 
 
-e = Entry(root, width=50, font=("Helvetica", 30))
-e.pack(padx=10, pady=10)
+e = Entry(root, width=17, font=("Helvetica", 30))
+e.grid(row = 0, column = 0, padx=10, pady=10)
 
 myButton = Button(root, text="Enter Your Name", command=functionClick)
-myButton.pack(pady=10)
+myButton.grid(row = 1, column = 0, pady=10)
 
-buttonDelete = Button(root, text="Delete Text", command=functionDelete)
-buttonDelete.pack(pady=10)
+# buttonDelete = Button(root, text="Delete Text", command=functionDelete)
+# buttonDelete.grid(row = 2, column = 0, pady=10)
 
 root.mainloop()
